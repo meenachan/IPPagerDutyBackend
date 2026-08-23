8c8ec8cb4c634810d105075bf7355f9454e80bc0
Meenakshi Chauhan <mcquitypie3@gmail.com>
2026-08-23 11:53:32 +0530

    init

---
+++ ipDeadlineTracker/src/main/java/com/IpPagerDuty/ipDeadlineTracker/IpDeadlineTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.IpPagerDuty.ipDeadlineTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IpDeadlineTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IpDeadlineTrackerApplication.class, args);
+	}
+
+}
---
+++ ipDeadlineTracker/src/test/java/com/IpPagerDuty/ipDeadlineTracker/IpDeadlineTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.IpPagerDuty.ipDeadlineTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IpDeadlineTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
